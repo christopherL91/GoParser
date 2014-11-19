8df250c224785e93512d806e9bbea00644dc458c
Christopher Lillthors <christopher.lillthors@gmail.com>
2014-11-19 02:03:02 +0100

    Added support for validation

--- parser.go
+++ parser.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/christopherL91/Parser/toki"
 	"io/ioutil"
 	"log"
 	"os"
-	"unsafe"
+	"regexp"
+	"strings"
 )
+
+type Instruction struct {
+	Name         string
+	Num          int
+	Color        string
+	Instructions []Instruction
+}
 
 const (
 	NUMBER toki.Token = iota + 1
@@ -15,22 +24,19 @@ const (
 	DOWN
 	FORW
 	LEFT
-	COMMENT
 	BACK
 	RIGHT
 	UP
 	COLORKEYWORD
 	REP
-	WHITESPACE
 	DOT
 	COLOR
+	OTHER
 )
 
 var (
 	defintions = []toki.Def{
-		{Token: COLOR, Pattern: `\#[A-F 0-9]{6}`},
 		{Token: STRING, Pattern: `\"`},
-		{Token: COMMENT, Pattern: "%.*\n"},
 		{Token: FORW, Pattern: `FORW\s+`},
 		{Token: LEFT, Pattern: `LEFT\s+`},
 		{Token: DOWN, Pattern: "DOWN"},
@@ -40,9 +46,18 @@ var (
 		{Token: COLORKEYWORD, Pattern: `COLOR\s+`},
 		{Token: RIGHT, Pattern: "RIGHT"},
 		{Token: UP, Pattern: "UP"},
-		{Token: DOT, Pattern: "."},
+		{Token: COLOR, Pattern: `\#[A-F 0-9]{6}`},
+		{Token: DOT, Pattern: `\.`},
+		{Token: OTHER, Pattern: `.*`},
 	}
+	removeComments = regexp.MustCompile("%[^\\n]*")
+	debug          bool
 )
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "Debuging")
+	flag.Parse()
+}
 
 func main() {
 	input, err := ioutil.ReadAll(os.Stdin) // Read input from stdin
@@ -50,80 +65,162 @@ func main() {
 		log.Fatal(err)
 	}
 	s := toki.NewScanner(defintions)
-	s.SetInput(string(input))
+	noComments := removeComments.ReplaceAllString(string(input), "")
+	s.SetInput(noComments)
 	buffer := []*toki.Result{} // Holds all the tokens.
 	for r := s.Next(); r.Token != toki.EOF; r = s.Next() {
 		buffer = append(buffer, r) // Append new token to list.
 	}
-	prettyPrint(buffer)
-	// validateBuffer(buffer)
+	if debug {
+		prettyPrint(buffer)
+		fmt.Println("Length of buffer:", len(buffer))
+	}
+	if err := validateBuffer(buffer); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	fmt.Println("The program is valid... Beginning evaluation...")
 }
 
 func prettyPrint(buffer []*toki.Result) {
 	for _, val := range buffer {
+		value := strings.TrimSpace(string(val.Value))
 		switch val.Token {
 		case NUMBER:
-			fmt.Println("NUMBER")
+			fmt.Println("NUMBER", value)
 		case DOWN:
-			fmt.Println("DOWN")
+			fmt.Println("DOWN", value)
 		case FORW:
-			fmt.Println("FORW")
+			fmt.Println("FORW", value)
 		case LEFT:
-			fmt.Println("LEFT")
-		case COMMENT:
-			fmt.Println("COMMENT")
+			fmt.Println("LEFT", value)
 		case BACK:
-			fmt.Println("BACK")
+			fmt.Println("BACK", value)
 		case RIGHT:
-			fmt.Println("RIGHT")
+			fmt.Println("RIGHT", value)
 		case UP:
-			fmt.Println("UP")
+			fmt.Println("UP", value)
 		case COLORKEYWORD:
-			fmt.Println("COLORKEYWORD")
+			fmt.Println("COLORKEYWORD", value)
 		case REP:
-			fmt.Println("REP")
-		case WHITESPACE:
-			fmt.Println("WHITESPACE")
+			fmt.Println("REP", value)
 		case DOT:
-			fmt.Println("DOT")
+			fmt.Println("DOT", value)
 		case STRING:
-			fmt.Println("STRING")
+			fmt.Println("STRING", value)
 		case COLOR:
-			fmt.Println("COLOR")
+			fmt.Println("COLOR", value)
 		}
 	}
+	fmt.Println()
 }
 
-func validateBuffer(buffer []*toki.Result) {
-	for index, token := range buffer {
-		switch token.Token {
-		case NUMBER:
-
-		case DOWN:
-
-		case FORW:
-
-		case LEFT:
-
-		case COMMENT:
-			// Do nothing
-		case BACK:
-
-		case RIGHT:
-
+func validateBuffer(buffer []*toki.Result) error {
+	// instructions := []Instruction{}
+L:
+	for i := 0; i < len(buffer); {
+		switch buffer[i].Token {
 		case UP:
-
+			fallthrough
+		case DOWN:
+			if len(buffer) <= i+1 {
+				return syntaxError(buffer[i].Pos.Line)
+			}
+			// Next token must be a DOT
+			if !(buffer[i+1].Token == DOT) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			// instruction := Instruction{
+			// 	Name: "DOWN",
+			// 	Num:  toInt(buffer[i+1].Value),
+			// }
+			// instruction = append(instructions, instruction)
+			i += 2
+		case FORW:
+			// Next token must be a NUMBER
+			if !(buffer[i+1].Token == NUMBER) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			if !(buffer[i+2].Token == DOT) {
+				return syntaxError(buffer[i+2].Pos.Line)
+			}
+			// instruction := Instruction{
+			// 	Name: "Forward",
+			// 	Num:  toInt(buffer[i+1].Value),
+			// }
+			// instructions = append(instructions, instruction)
+			i += 3
+		case LEFT:
+			if !(buffer[i+1].Token == NUMBER) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			if !(buffer[i+2].Token == DOT) {
+				return syntaxError(buffer[i+2].Pos.Line)
+			}
+			i += 3
+		case BACK:
+			if !(buffer[i+1].Token == NUMBER) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			if !(buffer[i+2].Token == DOT) {
+				return syntaxError(buffer[i+2].Pos.Line)
+			}
+			i += 3
+		case RIGHT:
+			if !(buffer[i+1].Token == NUMBER) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			if !(buffer[i+2].Token == DOT) {
+				return syntaxError(buffer[i+2].Pos.Line)
+			}
+			i += 3
 		case COLORKEYWORD:
-
+			// Next token must be COLOR
+			if !(buffer[i+1].Token == COLOR) {
+				return syntaxError(buffer[i+1].Pos.Line)
+			}
+			//and after that DOT
+			if !(buffer[i+2].Token == DOT) {
+				return syntaxError(buffer[i+2].Pos.Line)
+			}
+			// instructions = append(instructions, []Instruction{ColorInstruction{red: 255,green:0,blue:255}})
+			i += 3
 		case REP:
+			// Not implemented
+			break L
+			// 	if buffer[i+2].Token == STRING {
+			// 		i += 3
+			// 		for {
 
-		case WHITESPACE:
-
-		case DOT:
-
-		case STRING:
-
-		case COLOR:
+			// 			if buffer[i].Token == STRING {
+			// 				instruction := Instruction{
+			// 					Name:         "REP",
+			// 					Instructions: parsed_instructions,
+			// 				}
+			// 				break
+			// 			}
+			// 		}
+			// 	} else {
+			// 		instruction := Instruction{
+			// 			Name:         "REP",
+			// 			Instructions: []Instruction{parsed_instruction},
+			// 		}
+			// 	}
+			//
+		default:
+			// Found token that's invalid
+			return syntaxError(buffer[i].Pos.Line)
 		}
 	}
+	// Everything seems okey!
+	return nil
+}
+
+func evaluateProgram(program []Instruction) string {
+	// TODO
+	return ""
+}
+
+func syntaxError(line int) error {
+	return fmt.Errorf("Syntaxfel på rad %d", line)
 }
--- parser_test.go
+++ parser_test.go
@@ -7,10 +7,9 @@ import (
 	"testing"
 )
 
+// Functional program
 func TestExample1(t *testing.T) {
 	expected := []toki.Token{
-		COMMENT,
-		COMMENT,
 		DOWN,
 		DOT,
 		FORW,
@@ -38,28 +37,40 @@ func TestExample1(t *testing.T) {
 		NUMBER,
 		DOT,
 	}
-
 	input, err := ioutil.ReadFile("example1.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
 	s := toki.NewScanner(defintions)
-	s.SetInput(string(input))
-	Convey("Testing that example1.txt is a valid program", t, func() {
-		for _, e := range expected {
-			r := s.Next()
-			if e != r.Token {
-				t.Fatalf("expected %v, got %v", e, r.Token)
-			} else {
-				t.Log(r)
-			}
+	noComments := removeComments.ReplaceAllString(string(input), "")
+	s.SetInput(noComments)
+	buffer := []*toki.Result{} // Holds all the tokens.
+	Convey("Example1: Tokenize program using lexer", t, func() {
+		for r := s.Next(); r.Token != toki.EOF; r = s.Next() {
+			buffer = append(buffer, r) // Append new token to list.
 		}
+		So(buffer, ShouldNotBeEmpty)
+		Convey("Should be able to compare result with expected", func() {
+			if len(buffer) != len(expected) {
+				t.FailNow()
+			} else {
+				for i := 0; i < len(buffer); i++ {
+					if buffer[i].Token != expected[i] {
+						t.FailNow()
+					}
+				}
+			}
+		})
+		Convey("Program should be valid", func() {
+			err := validateBuffer(buffer)
+			So(err, ShouldBeNil)
+		})
 	})
 }
 
+// Functional program
 func TestExample2(t *testing.T) {
 	expected := []toki.Token{
-		COMMENT,
 		DOWN,
 		DOT,
 		UP,
@@ -68,9 +79,6 @@ func TestExample2(t *testing.T) {
 		DOT,
 		DOWN,
 		DOT,
-		COMMENT,
-		COMMENT,
-		COMMENT,
 		REP,
 		NUMBER,
 		STRING,
@@ -93,9 +101,7 @@ func TestExample2(t *testing.T) {
 		NUMBER,
 		DOT,
 		STRING,
-		COMMENT,
 		COLORKEYWORD,
-		COMMENT,
 		COLOR,
 		DOT,
 		REP,
@@ -104,21 +110,75 @@ func TestExample2(t *testing.T) {
 		NUMBER,
 		DOT,
 	}
-
 	input, err := ioutil.ReadFile("example2.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
 	s := toki.NewScanner(defintions)
-	s.SetInput(string(input))
-	Convey("Testing that example2.txt is a valid program", t, func() {
-		for _, e := range expected {
-			r := s.Next()
-			if e != r.Token {
-				t.Fatalf("expected %v, got %v", e, r.Token)
-			} else {
-				t.Log(r)
-			}
+	noComments := removeComments.ReplaceAllString(string(input), "")
+	s.SetInput(noComments)
+	buffer := []*toki.Result{} // Holds all the tokens.
+	Convey("Example2: Tokenize program using lexer", t, func() {
+		for r := s.Next(); r.Token != toki.EOF; r = s.Next() {
+			buffer = append(buffer, r) // Append new token to list.
 		}
+		So(buffer, ShouldNotBeEmpty)
+		Convey("Should be able to compare result with expected", func() {
+			if len(buffer) != len(expected) {
+				t.FailNow()
+			} else {
+				for i := 0; i < len(buffer); i++ {
+					if buffer[i].Token != expected[i] {
+						t.FailNow()
+					}
+				}
+			}
+		})
+		Convey("Program should be valid", func() {
+			err := validateBuffer(buffer)
+			So(err, ShouldBeNil)
+		})
+	})
+}
+
+// Broken program
+func TestExample3(t *testing.T) {
+	expected := []toki.Token{
+		COLORKEYWORD,
+		NUMBER,
+		FORW,
+		NUMBER,
+		DOT,
+	}
+	input, err := ioutil.ReadFile("example3.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := toki.NewScanner(defintions)
+	noComments := removeComments.ReplaceAllString(string(input), "")
+	s.SetInput(noComments)
+	buffer := []*toki.Result{} // Holds all the tokens.
+	Convey("Example3: Tokenize program using lexer", t, func() {
+		for r := s.Next(); r.Token != toki.EOF; r = s.Next() {
+			buffer = append(buffer, r) // Append new token to list.
+		}
+		t.Log("Buffer:", buffer)
+		So(buffer, ShouldNotBeEmpty)
+		t.Log("Length of buffer", len(buffer))
+		Convey("Should be able to compare result with expected", func() {
+			if len(buffer) != len(expected) {
+				t.FailNow()
+			} else {
+				for i := 0; i < len(buffer); i++ {
+					if buffer[i].Token != expected[i] {
+						t.FailNow()
+					}
+				}
+			}
+		})
+		Convey("Program should be valid", func() {
+			err := validateBuffer(buffer)
+			So(err, ShouldNotBeNil)
+		})
 	})
 }
